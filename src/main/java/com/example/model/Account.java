fde27288d7ea8198964da0cabf3f6f4673c8c235
Zelei Daniel <zelei.dan@gmail.com>
2017-04-01 23:22:37 +0200

    add account and role model

---
+++ src/main/java/com/example/model/Account.java
@@ -0,0 +1,64 @@
+package com.example.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Account {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(unique = true)
+	private String username;
+
+	private String password;
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Set<Role> roles;
+
+	public Account() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+}
---
+++ src/main/java/com/example/model/Role.java
@@ -0,0 +1,44 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+	@Id
+	private Long id;
+
+	private String code;
+
+	private String label;
+
+	public Role() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
