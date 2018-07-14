1d74061176e13c54bd35a9b589222f5eee9958aa
DanielZelei <zelei.dan@gmail.com>
2018-07-15 00:40:37 +0200

    fix sql problem with hsql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO privilege (privilege_id, name, description) VALUES (1, 'PRIVILEGE_ADMIN_READ', 'description for privilege admin read') ON CONFLICT DO NOTHING;
-INSERT INTO privilege (privilege_id, name, description) VALUES (2, 'PRIVILEGE_USER_READ', 'description for privilege user read') ON CONFLICT DO NOTHING;
+INSERT INTO privilege (privilege_id, name, description) VALUES (1, 'PRIVILEGE_ADMIN_READ', 'description for privilege admin read');
+INSERT INTO privilege (privilege_id, name, description) VALUES (2, 'PRIVILEGE_USER_READ', 'description for privilege user read');
 
-INSERT INTO role (role_id, name, description) VALUES (1, 'ROLE_ADMIN', 'description for role admin') ON CONFLICT DO NOTHING;
-INSERT INTO role (role_id, name, description) VALUES (2, 'ROLE_USER', 'description for role user') ON CONFLICT DO NOTHING;
+INSERT INTO role (role_id, name, description) VALUES (1, 'ROLE_ADMIN', 'description for role admin');
+INSERT INTO role (role_id, name, description) VALUES (2, 'ROLE_USER', 'description for role user');
 
-INSERT INTO role_privileges (role_role_id, privileges_privilege_id) VALUES (1, 1) ON CONFLICT DO NOTHING;
-INSERT INTO role_privileges (role_role_id, privileges_privilege_id) VALUES (2, 2) ON CONFLICT DO NOTHING;
+INSERT INTO role_privileges (role_role_id, privileges_privilege_id) VALUES (1, 1);
+INSERT INTO role_privileges (role_role_id, privileges_privilege_id) VALUES (2, 2);
 
-INSERT INTO account (id, enabled, username, password) VALUES (1, true, 'admin', '$2a$10$MTFVrdqbHOi.CCUhkrkZnOBdrZEfk3gzIUyZBdQvLWvdF/0pnkEO2') ON CONFLICT DO NOTHING;
-INSERT INTO account (id, enabled, username, password) VALUES (2, true, 'user', '$2a$10$6KDklkImZgGANWR8pDAwSexf6Bt4Z9I0nDiwdih9Q38HI4eAkWk0u') ON CONFLICT DO NOTHING;
+INSERT INTO account (id, enabled, username, password) VALUES (1, true, 'admin', '$2a$10$MTFVrdqbHOi.CCUhkrkZnOBdrZEfk3gzIUyZBdQvLWvdF/0pnkEO2');
+INSERT INTO account (id, enabled, username, password) VALUES (2, true, 'user', '$2a$10$6KDklkImZgGANWR8pDAwSexf6Bt4Z9I0nDiwdih9Q38HI4eAkWk0u');
 
-INSERT INTO account_roles (account_id, roles_role_id) VALUES (1, 1) ON CONFLICT DO NOTHING;
-INSERT INTO account_roles (account_id, roles_role_id) VALUES (2, 2) ON CONFLICT DO NOTHING;
+INSERT INTO account_roles (account_id, roles_role_id) VALUES (1, 1);
+INSERT INTO account_roles (account_id, roles_role_id) VALUES (2, 2);
